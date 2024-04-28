a8cbf3998b091eef037f57e99c2071c8e0b47715
Michael Mgebrishvili <mmd920802@gmail.com>
2024-04-28 16:25:21 +0300

    create funsctiom add and delete card

--- src/components/UserCard/UserTypes.ts
+++ src/components/UserCard/UserTypes.ts
@@ -5,30 +5,32 @@ export type user = {
     phone: string;
     birthday: string;
     address: string;
+    avatar: string;
 }
 
 export type UserCardProps = {
     user: user;
-    toggle: boolean
-    onDelete: () => void;
+    onDelete: (id: string) => void;
 }
 
 export type ResponseUser = {
-    result: {
-        name: { first: string, last: string };
-        email: string;
-        login: {
-            uuid: string;
-        };
-        dob: { date: string };
-        phone: string;
-        picture: {
-            large: string,
-        }
-        location: {
-            city: string,
-            state: string,
-            country: string,
-        }
+    name: { first: string, last: string };
+    email: string;
+    login: {
+        uuid: string;
+    };
+    dob: { date: string };
+    phone: string;
+    picture: {
+        large: string,
     }
+    location: {
+        city: string,
+        state: string,
+        country: string,
+    }
+}
+
+export type Response = {
+    results: ResponseUser[];
 }
---
+++ src/utils.ts
@@ -0,0 +1,37 @@
+import type { ResponseUser, user } from './components/UserCard/UserTypes';
+
+const formatDate = (dateString: string) => {
+    const months = [
+      "January", "February", "March", "April", "May", "June", "July",
+      "August", "September", "October", "November", "December"
+    ];
+  
+    const date = new Date(dateString);
+    const day = date.getDate();
+    const month = months[date.getMonth()];
+    const year = date.getFullYear();
+  
+    return `${day} ${month} ${year}`;
+  };
+
+const fieldSorting = (data: ResponseUser[]): user[] => {
+    const sortedData: user[] = [];
+
+    for (let card of data) {
+        sortedData.push(
+            {
+                id: card.login.uuid,
+                name: card.name.first + ' ' + card.name.last,
+                email: card.email,
+                phone: card.phone,
+                birthday: formatDate(card.dob.date),
+                address: card.location.city + ',' + card.location.state + ',' + card.location.country,
+                avatar: card.picture.large,
+            }
+        )
+    }
+
+    return sortedData;
+}
+
+export {fieldSorting}
